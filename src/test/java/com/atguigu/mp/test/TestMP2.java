8cb50c5b8a1e54a2144908d4fb4c753162427fca
gaohaoyou9413 <gaohaoyou9413@xzintl.com>
2021-01-07 14:35:25 +0800

    遴选2
    (cherry picked from commit f5488209d958d1d1b92404b761e92c7f4aeea765)

--- src/test/java/com/atguigu/mp/test/TestMP2.java
+++ src/test/java/com/atguigu/mp/test/TestMP2.java
@@ -64,7 +64,7 @@ public class TestMP2 {
         strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
         // strategy.setTablePrefix(new String[] { "tlog_", "tsys_" });// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        strategy.setInclude(new String[] { "tenant_product_custom_attr_value"}); // 需要生成的表
+        strategy.setInclude(new String[] { "tenant_store_single_product_multi_unit"}); // 需要生成的表
         // strategy.setExclude(new String[]{"test"}); // 排除生成的表
         mpg.setStrategy(strategy);
 
